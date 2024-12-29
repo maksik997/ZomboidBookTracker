a03e174fa9cd168303f540eb7aa74c04d835645e
Maksymilian <99613189+maksik997@users.noreply.github.com>
2024-12-29 21:48:51 +0100

    Tracker Service and handleAdd() method
    -- Added new method handleAdd()
    -- Added new method TrackerService#addBook()

--- src/main/java/pl/magzik/zomboidbooktracker/controller/TrackerController.java
+++ src/main/java/pl/magzik/zomboidbooktracker/controller/TrackerController.java
@@ -3,17 +3,28 @@ package pl.magzik.zomboidbooktracker.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.magzik.zomboidbooktracker.model.BookTableModel;
 import pl.magzik.zomboidbooktracker.model.TrackerModel;
+import pl.magzik.zomboidbooktracker.service.TrackerService;
+
+import java.util.Optional;
 
 public class TrackerController {
 
+    private static final Logger log = LoggerFactory.getLogger(TrackerController.class);
+
     private final TrackerModel model;
+
+    private final TrackerService service;
 
     public TrackerController() {
         this.model = new TrackerModel();
+        this.service = new TrackerService(model);
     }
 
     @FXML
@@ -56,21 +67,30 @@ public class TrackerController {
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 
         int n = 0;
-
         initializeLevel(levelOneColumn, n++);
         initializeLevel(levelTwoColumn, n++);
         initializeLevel(levelThreeColumn, n++);
         initializeLevel(levelFourColumn, n++);
-        initializeLevel(levelFiveColumn, n++);
+        initializeLevel(levelFiveColumn, n);
 
         bookTable.setItems(model.getBooks());
+    }
 
+    @FXML
+    public void handleAdd() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Enter a book name:");
+        dialog.setHeaderText("Enter a book name pwetty please:");
+        dialog.setContentText("Book name:");
+
+        Optional<String> result = dialog.showAndWait();
+
+        result.ifPresent(service::addBook);
     }
 
     private void initializeLevel(TableColumn<BookTableModel, Boolean> levelColumn, int n) {
         levelColumn.setCellValueFactory(p -> p.getValue().getLevel().get(n));
         levelColumn.setCellFactory(CheckBoxTableCell.forTableColumn(levelColumn));
-
     }
 
 }
--- src/main/java/pl/magzik/zomboidbooktracker/service/TrackerService.java
+++ src/main/java/pl/magzik/zomboidbooktracker/service/TrackerService.java
@@ -1,4 +1,20 @@
 package pl.magzik.zomboidbooktracker.service;
 
+import pl.magzik.zomboidbooktracker.model.TrackerModel;
+
 public class TrackerService {
+
+    private final TrackerModel model;
+
+    public TrackerService(TrackerModel model) {
+        this.model = model;
+    }
+
+    /**
+     * This method should: add new book to the model class.
+     * */
+    public void addBook(String name) {
+        // PLEASE @TCPJaglak implement this method.
+    }
+
 }
